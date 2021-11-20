6ff2fe843cd0bbf5746810c3eb1eb97077ede7fb
liminggui <289657726@qq.com>
2021-11-20 18:42:56 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
-        System.out.println("hello Git hot-fix");
+        System.out.println("Hello Git4");
+        System.out.println("hot-fix test!");
     }
 }
